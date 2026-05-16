9ff2061b766d9911e5b7cb6420a5e2e8887e9945
MG <gmelina69@gmail.com>
2026-05-16 17:25:19 +0400

    Добавить утилитный класс DataHelper.java

---
+++ src/test/java/ru/netology/data/DataHelper.java
@@ -0,0 +1,47 @@
+package ru.netology.data;
+
+import lombok.Value;
+
+public class DataHelper {
+
+    private DataHelper() {
+
+    }
+
+    public static AuthInfo getAuthInfo() {
+        return new AuthInfo("vasya", "qwerty123");
+    }
+
+    public static VerifCode getVerifCode() {
+        return new VerifCode("12345");
+    }
+
+    public static Card getFirstCardInfo() {
+        return new Card("5559 0000 0000 0001", "92df3f1c-a033-48e6-8390-206f6b1f56c0");
+    }
+
+    public static Card getSecondCardInfo() {
+        return new Card("5559 0000 0000 0002", "0f3f5c2a-249e-4c3d-8287-09f7a039391d");
+    }
+
+    public static Card getWrongCardInfo() {
+        return new Card("5559 0000 0000 1111", "0f3f5c2a-249e-4c3d-8287-09f7a039355d");
+    }
+
+    @Value
+    public static class AuthInfo {
+        String login;
+        String password;
+    }
+
+    @Value
+    public static class VerifCode {
+        String code;
+    }
+
+    @Value
+    public static class Card {
+        String number;
+        String testId;
+    }
+}
